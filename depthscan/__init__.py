75c338d838d3093e7e852df15f0fb8d1e8fa269c
Chino Franco <chino.franco@gmail.com>
2022-12-25 23:11:10 +0900

    Added config parsing

--- depthscan/__init__.py
+++ depthscan/__init__.py
@@ -1,1 +1,16 @@
+import argparse
 from .camera import DepthScanner
+
+def config():    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", "-m",
+                        type=str,  
+                        default="standard", 
+                        help="Set to \'live\' for live depth-capture, or \'standard\' otherwise")
+    parser.add_argument("--camera", "-c",
+                        type=int, 
+                        default=0, 
+                        help="Webcam port to capture, default is 0")
+    args = parser.parse_args()
+    return args
+    
