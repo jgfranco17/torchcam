b95f37bcd4911032b096b8d5f56dbf3d85a7f2af
Chino Franco <chino.franco@gmail.com>
2022-12-26 00:41:57 +0900

    Use higher-accuracy model in standard mode

--- depthscan/camera.py
+++ depthscan/camera.py
@@ -12,18 +12,18 @@ class DepthScanner(object):
         self.is_running = False
         
         # Configure PyTorch MiDaS
-        self.model_type = "MiDaS_small"
-        self.model = torch.hub.load("intel-isl/MiDaS", self.model_type)
-        self.__device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-        self.model.to(self.device)
-        self.model.eval()
-        midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
-        self.transform = midas_transforms.small_transform
         modes = {
             "standard": False,
             "live": True
         }
         self.live_render = modes.get(mode, "standard")
+        self.model_type = "MiDaS_small" if self.live_render else "DPT_Large"
+        self.model = torch.hub.load("intel-isl/MiDaS", self.model_type)
+        self.__device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.model.to(self.device)
+        self.model.eval()
+        midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+        self.transform = midas_transforms.small_transform if self.live_render else midas_transforms.dpt_transform
         
     def __repr__(self) -> str:
         return f'<DepthScanner | camera={self.camera_num}, device={str(self.__device).upper()}>'
