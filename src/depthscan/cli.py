b3ae0d004a25a3986b5fd7217d612eacba5e71dc
Chino Franco <chino.franco@gmail.com>
2023-01-08 14:04:33 +0900

    renamed heatcam command to "live"

--- src/depthscan/cli.py
+++ src/depthscan/cli.py
@@ -4,6 +4,6 @@ from depthscan import DepthScanner
 app = Typer()
 
 @app.command()
-def heatcam(camera:int=0, scale:float=1.0, color:str="hot"):
+def live(camera:int=0, scale:float=1.0, color:str="hot"):
     scanner = DepthScanner(camera=camera, mode="live", scale=scale, color=color)
     scanner.run()
