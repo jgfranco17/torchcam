cd799d66fd219c3c0240ca21170ff99f8b1de33f
jgfranco17 <chino.franco@gmail.com>
2023-04-25 15:23:18 +0900

    Added config args

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
-from depthscan import config, DepthScanner
+import argparse
+from depthscan.camera import DepthScanner
+
+
+def config():    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", "-m",
+                        type=str,  
+                        default="standard", 
+                        help="Set to \'live\' for live depth-capture, or \'standard\' otherwise")
+    parser.add_argument("--camera", "-c",
+                        type=int, 
+                        default=0, 
+                        help="Webcam port to capture, default is 0")
+    parser.add_argument("--window", "-w",
+                        type=float, 
+                        default=1.0, 
+                        help="Display window scale, default is 1")
+    parser.add_argument("--style", "-s",
+                        type=str, 
+                        default="hot", 
+                        help="Colormap styling")
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == "__main__":
