08b3acb76305cbfc90ac9d864dc2289bf706895f
jgfranco17 <chino.franco@gmail.com>
2023-05-17 21:44:55 +0900

    Used config from package

--- app.py
+++ app.py
@@ -1,26 +1,6 @@
 import argparse
-from depthscan.camera import DepthCamera
-
-
-def config():    
-    parser = argparse.ArgumentParser()
-    parser.add_argument("mode",
-                        type=str,
-                        help="Set to \'live\' for live depth-capture, or \'standard\' otherwise")
-    parser.add_argument("--camera", "-c",
-                        type=int, 
-                        default=0, 
-                        help="Webcam port to capture, default is 0")
-    parser.add_argument("--window", "-w",
-                        type=float, 
-                        default=1.0, 
-                        help="Display window scale, default is 1")
-    parser.add_argument("--style", "-s",
-                        type=str, 
-                        default="hot", 
-                        help="Colormap styling")
-    args = parser.parse_args()
-    return args
+from torchcam.camera import DepthCamera
+from torchcam.cli import config
 
 
 if __name__ == "__main__":
