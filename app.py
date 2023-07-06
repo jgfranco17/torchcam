975d1ddc80af5fd548c505f294ac43c72c2370f6
Chino Franco <chino.franco@gmail.com>
2023-07-06 11:43:00 +0900

    Updated imported functions

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import argparse
 from torchcam.camera import DepthCamera
-from torchcam.cli import config
+from torchcam.cli import get_configs
 
 
 if __name__ == "__main__":
-    args = config()
+    args = get_configs()
     scanner = DepthCamera(camera=args.camera, mode=args.mode, scale=args.window, color=args.style)
     scanner.run()
     
